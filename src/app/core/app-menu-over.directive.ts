83652dc1ac2fb9087083a31abd2a2852bf86f1d0
Igor <igor.a.silva@capgemini.com>
2020-03-25 14:59:22 -0300

    Ajustes

--- src/app/core/app-menu-over.directive.ts
+++ src/app/core/app-menu-over.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, HostListener, ElementRef, Renderer2, Output, EventEmitter } from '@angular/core';
+import { Directive, HostListener, Output, EventEmitter, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAppMenuOver]'
 })
 export class AppMenuOverDirective {
+  @Input() sideNav;
+  @Output() toggleMenu: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Output() toggle: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-    // this.ChangeBgColor('red');
+  constructor() {
   }
   
-  @HostListener('mouseover') onMouseOver() {
-    this.toggle.emit(true);
-    // this.renderer.setElementStyle(this.el.nativeElement, 'color', color);
+  @HostListener('mouseover') onMouseOver(eventData: Event) {
+    this.sideNav.toggle();
   }
+
+  // @HostListener('mouseleave') onMouseLeave(eventData: Event) {
+  //   this.sideNav.toggle();
+  // }
 
 }

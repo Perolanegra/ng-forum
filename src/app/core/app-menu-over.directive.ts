20b4245c9b35448d2eb671746dd9e867138d192d
Igor <igor.a.silva@capgemini.com>
2020-03-27 15:13:12 -0300

    Ajustes no Menu

--- src/app/core/app-menu-over.directive.ts
+++ src/app/core/app-menu-over.directive.ts
@@ -10,10 +10,16 @@ export class AppMenuOverDirective {
   @HostListener('mouseenter') onMouseOver(eventData: Event): void {
     this.renderer.setStyle(this.elRef.nativeElement, 'width', '296px');
     const nativeElement: HTMLElement = this.elRef.nativeElement;
+   
 
     this.renderer.setStyle(nativeElement.querySelector('.div-img-background'), 'width', '120px');
     this.renderer.setStyle(nativeElement.querySelector('.div-img-background'), 'height', '119px');
 
+
+    this.renderer.setStyle(nativeElement.querySelector('.div-img-center-above'), 'height', '250px');
+
+    this.renderer.setStyle(nativeElement.querySelector('.img-logo-menu'), 'max-width', '130px');
+    
 
     this.renderer.setStyle(nativeElement.querySelector('.div-img-center'), 'bottom', '17vh');
 
@@ -35,13 +41,14 @@ export class AppMenuOverDirective {
     this.renderer.setStyle(nativeElement.querySelector('.div-img-background'), 'margin-top', '15%');
     this.renderer.setStyle(nativeElement.querySelector('.div-img-background'), 'height', '65px');
 
+    this.renderer.setStyle(nativeElement.querySelector('.div-img-center-above'), 'height', '200px');
+
+    this.renderer.setStyle(nativeElement.querySelector('.img-logo-menu'), 'max-width', '90px');
+
     this.renderer.setStyle(nativeElement.querySelector('.div-img-center'), 'bottom', '9vh');
 
     this.renderer.setStyle(nativeElement.querySelector('.img-menu-over'), 'max-width', '55px');
     this.renderer.setStyle(nativeElement.querySelector('.img-menu-over'), 'max-height', '55px');
-    
-
-    // this.renderer.setStyle(nativeElement.querySelector('.img-menu-over'), 'height', '60px');
   }
 
 }

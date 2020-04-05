5f0cdf44b223fb89f739adcafd4eb5607d70066f
kalito <igoralves@devbaiano.com.br>
2020-04-05 12:09:13 -0300

    Implementado icones

--- src/app/core/fillerNav.ts
+++ src/app/core/fillerNav.ts
@@ -2,9 +2,9 @@ export class FillerNav {
     constructor() {
         return new Object({
             routes: [
-                { name: 'Início', isActive: true, path: 'home' },
-                { name: 'Perfil', isActive: false, path: 'profile' },
-                { name: 'My Issues', isActive: false, path: 'my-stuff' },
+                { name: 'Início', isActive: true, path: 'home', img: '../../assets/imgs/logo.png' },
+                { name: 'Perfil', isActive: false, path: 'profile', img: '../../assets/svg/profile.svg' },
+                { name: 'My Issues', isActive: false, path: 'my-stuff', img: '../../assets/svg/profile.svg' },
             ]
         });
     }

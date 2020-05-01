5fd5c2499df33f81ab295674b1163f223e2de842
kalito <igoralves@devbaiano.com.br>
2020-05-01 12:41:59 -0300

    Saving

---
+++ src/assets/dumps/dump-ng.sql
@@ -0,0 +1,15 @@
+/**
+## Execute para Inserir dados na tabela nivel
+
+INSERT INTO nivel(`id`, `description`)
+VALUES (null, 'ADM');
+
+INSERT INTO nivel(`id`, `description`)
+VALUES (null, 'Mago');
+
+INSERT INTO nivel(`id`, `description`)
+VALUES (null, 'Moderador');
+
+INSERT INTO nivel(`id`, `description`)
+VALUES (null, 'Noob');
+*/

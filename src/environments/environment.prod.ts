a0953fb81cca16c29d4d768823c5da1f2c2b7dc7
perolanegra <igoralves@devbaiano.com.br>
2020-05-23 15:48:18 -0300

    gitignore fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,5 @@
 export const environment = {
   production: true,
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyB3WDZnewXJ1ndSLvBstVqdecVgjJDObv0",
-  //   authDomain: "ng-forum-2988f.firebaseapp.com",
-  //   databaseURL: "https://ng-forum-2988f.firebaseio.com",
-  //   projectId: "ng-forum-2988f",
-  //   storageBucket: "ng-forum-2988f.appspot.com",
-  //   messagingSenderId: "701159976222",
-  //   appId: "1:701159976222:web:ad48eb8652bde1046aad0f",
-  //   measurementId: "G-8684DCEZWZ"
-  // },
-  server: 'https://ngba-rest.herokuapp.com/',
+  server: 'https://ngba-rest.herokuapp.com',
   release: '1.0.0'
 };

0204e21713d88f045ba7bb99406474ea5431a706
perolanegra <igoralves@devbaiano.com.br>
2020-06-01 17:41:59 -0300

    Ajustes dumps

--- src/assets/dumps/dump-ng.sql
+++ src/assets/dumps/dump-ng.sql
@@ -17,11 +17,26 @@ VALUES (null, 'Noob');
 /**
 ## Execute para inserir dados na tabela user
 
-
-*/
-
-
 INSERT INTO user(`id`, `id_nivel`, `name`, `ddd`, `phone`, `created_at`,
  `deleted_at`, `updated_at`, `username`, `password`, `lastName`, `statusMsg` , `email`, `photoURL`, `birthDate`)
 VALUES (null, 1, 'Igor', '071', '993337275', current_timestamp, '', '', 'perolanegra', 'ge7OBMv703BbeQZNnB52OA==', 'Alves', '', 'igoralves@devbaiano.com.br',
 '', '');
+
+######
+
+## Execute para inserir dados na tabela tags
+INSERT INTO tags VALUES (NULL, 'bug', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'implementation', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'created', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'closed', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'reopen', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'error', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'resolved', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'question', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'hot', current_timestamp(),'', '');
+INSERT INTO tags VALUES (NULL, 'deprecated', current_timestamp(),'', '');
+######
+*/
+
+
+

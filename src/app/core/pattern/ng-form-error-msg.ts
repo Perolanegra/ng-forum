d195a11a7b46c7f7e10d295e2837381e22dd3bff
perolanegra <igoralves@devbaiano.com.br>
2020-08-06 12:29:22 -0300

    OK

--- src/app/core/pattern/ng-form-error-msg.ts
+++ src/app/core/pattern/ng-form-error-msg.ts
@@ -1,8 +1,8 @@
 export class NgFormErrorMesssage {
-    public getMessages(): Array<string> {
+    public getMessages(qtdCharRule = 8): Array<string> {
         return [
             'O campo é obrigatório.', // id: 0
-            'Mínimo de 8 caracteres.', // id: 1
+            `Mínimo de ${qtdCharRule} caracteres.`, // id: 1
             'Não pode conter espaços em branco.',  // id: 2
             'Senhas não coincidem.', // id: 3,
             'Mínimo de 2 caracteres.', // id: 4,

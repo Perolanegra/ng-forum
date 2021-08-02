9c32ea0199e9ab71f6b922db9c58e7a96e1b9e21
perolanegra <igoralves@devbaiano.com.br>
2021-08-01 22:49:22 -0300

    Ref de server add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server: "localhost",
+  server: "https://ngba-rest.herokuapp.com",
   release: "1.0.0",
   prefixImg: "/assets/imgs/",
   prefixAnimationJSON: "/assets/animations/json/",

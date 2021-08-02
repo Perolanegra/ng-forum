9507707ec76ab3167afa05f81859a5a885eaa46b
perolanegra <igoralves@devbaiano.com.br>
2021-08-01 22:28:22 -0300

    Mudando referencia de assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  server: 'localhost',
-  release: '1.0.0',
-  prefixImg: '/ng-forum/assets/imgs/',
-  prefixAnimationJSON: 'ng-forum/assets/animations/json/',
+  server: "localhost",
+  release: "1.0.0",
+  prefixImg: "/assets/imgs/",
+  prefixAnimationJSON: "/assets/animations/json/",
 };
